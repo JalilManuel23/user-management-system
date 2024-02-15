7db3bb16d0b68d096f1628acd11e426aea7c8a73
J. Manuel López <35846819+JalilManuel23@users.noreply.github.com>
2024-02-15 08:39:17 -0600

    set favicon and title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,10 @@
 <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>User Management System</title>
+        <link rel="icon" href="/user.png">
+    </head>
     <body>
         <div id="app"></div>
         @vite("resources/js/app.js")

23d9b97e44fd227100940ef1f0200931fd2b56cd
J. Manuel López <35846819+JalilManuel23@users.noreply.github.com>
2024-02-15 15:49:23 -0600

    User's list

--- resources/js/views/users/Layout.vue
+++ resources/js/views/users/Layout.vue
@@ -8,14 +8,79 @@ const logout = () => {
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <p @click="logout">Logout</p>
-        </div>
-        <div class="row">
-            <div class="col-sm-8 offset-sm-2 mt-5">
-                <router-view></router-view>
-            </div>
-        </div>
-    </div>
+    <v-app>
+        <!-- Navbar -->
+        <v-app-bar app color="primary">
+            <v-toolbar-title>User Management System</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn @click="logout">Logout</v-btn>
+        </v-app-bar>
+
+        <!-- Contenido Principal -->
+        <v-main>
+            <v-container fluid>
+                <!-- Layout con fila y columna -->
+                <v-row>
+                    <!-- Columna de Opciones -->
+                    <v-col cols="2">
+                        <v-card class="fill-height">
+                            <v-list>
+                                <v-list-item link>
+                                    <v-list-item-icon>
+                                        <v-icon>mdi-home</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-title
+                                        >Inicio</v-list-item-title
+                                    >
+                                </v-list-item>
+                                <v-list-item link>
+                                    <v-list-item-icon>
+                                        <v-icon>mdi-account</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-title
+                                        >Mi Perfil</v-list-item-title
+                                    >
+                                </v-list-item>
+                                <v-list-item link>
+                                    <v-list-item-icon>
+                                        <v-icon>mdi-settings</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-title
+                                        >Configuración</v-list-item-title
+                                    >
+                                </v-list-item>
+                            </v-list>
+                        </v-card>
+                    </v-col>
+
+                    <!-- Contenido -->
+                    <v-col cols="10">
+                        <!-- Contenido dinámico de la página -->
+                        <v-card class="fill-height">
+                            <v-card-text>
+                                <!-- Aquí va el contenido dinámico de la página -->
+                                <router-view></router-view>
+                            </v-card-text>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
+
+<script>
+export default {
+    methods: {
+        logout() {
+            // Aquí puedes agregar la lógica para cerrar sesión
+        },
+    },
+};
+</script>
+
+<style scoped>
+.fill-height {
+    height: 100%;
+}
+</style>
--- resources/js/views/users/List.vue
+++ resources/js/views/users/List.vue
@@ -1,56 +1,33 @@
-<script setup>
-import { storeToRefs } from "pinia";
+<template>
+    <v-app>
+        <v-container>
+            <v-data-table :headers="headers" :items="users"></v-data-table>
+        </v-container>
+    </v-app>
+</template>
 
+<script>
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
 import { useUsersStore } from "@/stores";
 
-const usersStore = useUsersStore();
-const { users } = storeToRefs(usersStore);
+export const headers = [
+    { text: "ID", value: "id" },
+    { text: "Nombre", value: "name" },
+    { text: "Email", value: "email" },
+];
 
-usersStore.getAll();
+export default {
+    setup() {
+        const usersStore = useUsersStore();
+
+        const { users } = storeToRefs(usersStore);
+
+        usersStore.getAll();
+
+        const filter = ref("users");
+
+        return { usersStore, users };
+    },
+};
 </script>
-
-<template>
-    <h1>Users</h1>
-
-    <router-link to="/users/add" class="btn btn-sm btn-success mb-2"
-        >Add User</router-link
-    >
-
-    <table>
-        <thead>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Actions</th>
-        </thead>
-    </table>
-
-    <tbody>
-        <template v-if="users.length">
-            <tr v-for="user in users" :key="user.id">
-                <td>{{ user.id }}</td>
-                <td>{{ user.name }}</td>
-                <td>{{ user.email }}</td>
-                <td style="white-space: nowrap">
-                    <router-link
-                        @click="usersStore.setActiveUser(user)"
-                        :to="`/users/edit/${user.id}`"
-                        class="btn btn-sm btn-primary mr-1"
-                        >Edit</router-link
-                    >
-                    <button
-                        @click="usersStore.delete(user.id)"
-                        class="btn btn-sm btn-danger btn-delete-user"
-                        :disabled="user.isDeleting"
-                    >
-                        <span
-                            v-if="user.isDeleting"
-                            class="spinner-border spinner-border-sm"
-                        ></span>
-                        <span v-else>Delete</span>
-                    </button>
-                </td>
-            </tr>
-        </template>
-    </tbody>
-</template>
